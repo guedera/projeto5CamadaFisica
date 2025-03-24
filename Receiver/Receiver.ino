efa2d79af06f09bce54eb310e4ab19cc6531536a
guedera <guilhermegg5@al.insper.edu.br>
2025-03-24 11:05:02 -0300

    quickpush

--- Receiver/Receiver.ino
+++ Receiver/Receiver.ino
@@ -2,40 +2,51 @@ const int RX_PIN = 8; // Pino digital para recepção
 const int BAUDRATE = 9600;
 const int BIT_DELAY = 1000000 / BAUDRATE; // Tempo entre bits em microssegundos
 
+unsigned long lastReceiveTime = 0;
+boolean isReceiving = false;
+int currentBit = 0;
+byte receivedData = 0;
+byte parityBit = 0;
+
 void setup() {
     pinMode(RX_PIN, INPUT);
     Serial.begin(9600);
 }
 
 void loop() {
-    char receivedChar = receiveUART();
-    Serial.print("Caractere recebido: ");
-    Serial.println(receivedChar);
-    delay(1000); // Apenas para espaçar as leituras no monitor serial
-}
-
-char receiveUART() {
-    while (digitalRead(RX_PIN) == HIGH); // Espera pelo bit de start
-    delayMicroseconds(BIT_DELAY * 1.5); // Aguarda no meio do primeiro bit
+    unsigned long currentTime = micros();
     
-    byte data = 0;
-    for (int i = 0; i < 8; i++) {
-        data |= (digitalRead(RX_PIN) << i);
-        delayMicroseconds(BIT_DELAY);
+    if (!isReceiving && digitalRead(RX_PIN) == LOW) {
+        isReceiving = true;
+        lastReceiveTime = currentTime;
+        currentBit = -1;
     }
     
-    byte parity = digitalRead(RX_PIN); // Lê bit de paridade
-    delayMicroseconds(BIT_DELAY);
-    
-    digitalRead(RX_PIN); // Lê bit de stop (ignorado, mas necessário)
-    delayMicroseconds(BIT_DELAY);
-    
-    // Verificação de paridade (opcional, depende do critério de erro permitido)
-    if (calcParity(data) != parity) {
-        Serial.println("Erro de paridade!");
+    if (isReceiving && (currentTime - lastReceiveTime >= BIT_DELAY)) {
+        lastReceiveTime = currentTime;
+        
+        if (currentBit == -1) {
+            currentBit++;
+        } 
+        else if (currentBit >= 0 && currentBit < 8) {
+            receivedData |= (digitalRead(RX_PIN) << currentBit);
+            currentBit++;
+        } 
+        else if (currentBit == 8) {
+            parityBit = digitalRead(RX_PIN);
+            currentBit++;
+        } 
+        else if (currentBit == 9) {
+            isReceiving = false;
+            if (calcParity(receivedData) != parityBit) {
+                Serial.println("Erro de paridade!");
+            } else {
+                Serial.print("Caractere recebido: ");
+                Serial.println((char)receivedData);
+            }
+            receivedData = 0;
+        }
     }
-    
-    return data;
 }
 
 byte calcParity(byte data) {
